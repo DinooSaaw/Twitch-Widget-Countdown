c43538cc96d6803eb1faa1bb291c912b2095e622
DinooSaaw <57213778+DinooSaaw@users.noreply.github.com>
2024-11-22 23:03:31 +1100

    added timestamp to log message

--- js/lib/generalFunc.js
+++ js/lib/generalFunc.js
@@ -7,9 +7,22 @@ const randomInRange = (min, max) => {
 };
 
 const logMessage = (module, message) => {
-    console.log(`[${module}]: ${message}`);
+    console.log(`{${getTimestamp()}}: [${module}]: ${message}`);
 };
 
 const logObject = (module, object) => {
-    console.log(`[${module}]: `, object);
+    console.log(`{${getTimestamp()}}: [${module}]: `, object);
 };
+
+const getTimestamp = () => {
+    try {
+      const now = new Date();
+      const hours = now.getHours().toString().padStart(2, "0");
+      const minutes = now.getMinutes().toString().padStart(2, "0");
+      const seconds = now.getSeconds().toString().padStart(2, "0");
+      return `${hours}:${minutes}:${seconds}`;
+    } catch (error) {
+      console.error("Error occurred while getting timestamp:", error);
+      return "!00:00:00";
+    }
+  };
